de5aa4596174e6ff9ec386177d51adad6b872b03
Anis0000 <38005790+Anis0000@users.noreply.github.com>
2018-04-10 23:35:36 +0200

    3

---
+++ compter_les_mots.rb
@@ -0,0 +1,10 @@
+def jean_michel_data(string, dictionary)
+	frequencies = Hash.new
+	dictionary.each do |substring| 
+		frequencies[substring] = string.scan(/(?=#{substring})/).count
+		frequencies.delete(substring) if frequencies[substring] == 0
+	end
+	frequencies
+end
+
+puts jean_michel_data("below", ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"])
